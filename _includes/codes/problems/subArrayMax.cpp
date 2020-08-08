bb75d8841b96c19e4a75efd26d043308f618456e
Canh Do Minh <minhcanh99@gmail.com>
2020-08-09 00:28:29 +0900

    Sub array maximum problem

---
+++ _includes/codes/problems/subArrayMax.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n = 9;
+int arr[9] = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+
+void findSubArrayMax() {
+    int best = INT_MIN, sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum = max(arr[i], sum + arr[i]);
+        best = max(best, sum);
+    }
+    cout << best << "\n";
+}
+
+void findSubArrayMaxWithIndices() {
+    int best = INT_MIN, sum = 0;
+    int best_start = 0, best_end = 0, current_start = 0;
+    for (int i = 0; i < n; i++) {
+        if (sum + arr[i] < arr[i]) {
+            current_start = i;
+            sum = arr[i];
+        } else {
+            sum += arr[i];
+        }
+
+        if (best < sum) {
+            best = sum;
+            best_start = current_start;
+            best_end = i;
+        }
+        best = max(best, sum);
+    }
+    cout << best << "\n";
+    cout << "start from " << best_start << " to " << best_end << "\n";
+}
+
+int main() {
+    findSubArrayMax();
+    findSubArrayMaxWithIndices();
+}
---
+++ _posts/2020-08-09-c++-template-code.md
@@ -0,0 +1,12 @@
+---
+title: C++ template code
+date: 2020-08-09 00:18
+category:
+    - Programming
+    - Data Structures and Algorithms
+tags: [C++]
+---
+C++ template code as reference. We are updating ...
+{% highlight c++ %}
+{% include codes/templates/cpp-template-code.cpp %}
+{% endhighlight c++ %}
