16ab282e5802ab0ce229d2e451aaa1c5eba5bb92
Canh Minh Do <minhcanh99@gmail.com>
2025-09-28 11:28:12 +0900

    Update papers

--- run-merge.sh
+++ run-merge.sh
@@ -1,1 +1,1 @@
-cat _bibliography/conferences.bib _bibliography/journals.bib > _bibliography/papers.bib
+(cat _bibliography/conferences.bib; printf "\n\n"; cat _bibliography/journals.bib) > _bibliography/papers.bib
